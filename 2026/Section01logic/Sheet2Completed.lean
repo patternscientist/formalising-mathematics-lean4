01a8d4c90928e50ca3eb0a91e2ad00859648aee4
Donald Poindexter <noitcartsba@proton.me>
2026-02-14 21:26:45 -0800

    Complete Sheet 2 of Section01 Logic; rename Sheet2.lean => Sheet2Completed.lean

---
+++ 2026/Section01logic/Sheet2Completed.lean
@@ -0,0 +1,76 @@
+/-
+Copyright (c) 2025 Bhavik Mehta. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Bhavik Mehta, Kevin Buzzard
+-/
+import Mathlib.Tactic -- imports all the Lean tactics
+
+/-!
+
+# Logic in Lean, example sheet 2 : `True` and `False`
+
+We learn about the `True` and `False` propositions.
+
+## Tactics you will need
+
+To solve the levels on this sheet you will need to know all previous
+tactics, plus the following two new ones. Check out their explanations
+in the course book. Or just try them out and hover over them to see
+if you can understand what's going on.
+
+* `trivial`
+* `exfalso`
+
+-/
+
+
+-- Throughout this sheet, `P`, `Q` and `R` will denote propositions.
+variable (P Q R : Prop)
+
+example : True := by
+  trivial
+
+example : True → True := by
+  intro h
+  trivial
+
+example : False → True := by
+  intro h
+  cases h
+
+example : False → False := by
+  intro h
+  exact h
+
+example : (True → False) → False := by
+  intro h1
+  apply h1
+  trivial
+
+example : False → P := by
+  intro h
+  exfalso
+  exact h
+
+example : True → False → True → False → True → False := by
+  intro _ h _ _ _
+  exact h
+
+example : P → (P → False) → False := by
+  intro hP hPF
+  apply hPF
+  exact hP
+
+example : (P → False) → P → Q := by
+  intro hPF hP
+  exfalso
+  apply hPF
+  exact hP
+
+example : (True → False) → P := by
+  intro h1
+  have h3 : False := by
+    apply h1
+    trivial
+  exfalso
+  exact h3
